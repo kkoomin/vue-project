645230891c32d10373b7cab1cd57b2b316ca5121
kkoomin <kkooming2@gmail.com>
2020-06-24 14:31:26 +0900

    create imgur-uploader

---
+++ imgur-uploader/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>HOME</h1>
+    <p>This is a homepage</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+};
+</script>
